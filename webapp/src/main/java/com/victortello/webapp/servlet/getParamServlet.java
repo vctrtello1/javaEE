03dd0c8f5ea8466dba6865251f9324924bd7e44e
Victor Hugo Tello <victorhugotello@hotmail.com>
2022-01-31 21:43:04 -0600

    update pom.xml

---
+++ webapp/src/main/java/com/victortello/webapp/servlet/getParamServlet.java
@@ -0,0 +1,42 @@
+package com.victortello.webapp.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/getParam")
+public class getParamServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req,
+            HttpServletResponse resp) throws ServletException, IOException {
+
+        String saludo = req.getParameter("saludo");
+
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+
+        out.print("<!DOCTYPE html>");
+
+        out.print("<html>");
+        out.print("<head>");
+
+        out.print("<meta charset=\"UTF-8\">");
+        out.print("<title>$title$</title>");
+
+        out.print("</head>");
+
+        out.print("<body>");
+        out.print("<h2>$title$</h2>");
+        out.print("<h2>hola" + saludo + "</h2>");
+        out.print("</body>");
+        out.print("</html>");
+    }
+
+}
